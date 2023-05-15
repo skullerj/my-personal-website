9947eb5fc6553996c3f8f499920af2d01ff55703
Juan Rosero <juan@rosero.dev>
2023-05-15 23:35:07 +0100

    feat: add prettier and eslint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
