9cfee6a40050cfe3b2218ea31871d8b71b60768e
Juan Rosero <juan@rosero.dev>
2025-03-15 09:12:37 +0000

    feat: update colors to use oklch tooling

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,3 @@
-function withOpacity(variableName) {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `rgba(var(${variableName}), ${opacityValue})`;
-    }
-    return `rgb(var(${variableName}))`;
-  };
-}
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
@@ -19,36 +10,36 @@ module.exports = {
 
     textColor: {
       skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
-        inverted: withOpacity("--color-fill"),
+        base: "var(--color-text-base)",
+        accent: "var(--color-accent)",
+        inverted: "var(--color-fill)",
       },
     },
     backgroundColor: {
       skin: {
-        fill: withOpacity("--color-fill"),
-        accent: withOpacity("--color-accent"),
-        inverted: withOpacity("--color-text-base"),
-        card: withOpacity("--color-card"),
-        "card-muted": withOpacity("--color-card-muted"),
+        fill: "var(--color-fill)",
+        accent: "var(--color-accent)",
+        inverted: "var(--color-text-base)",
+        card: "var(--color-card)",
+        "card-muted": "var(--color-card-muted)",
       },
     },
     outlineColor: {
       skin: {
-        fill: withOpacity("--color-accent"),
+        fill: "var(--color-accent)",
       },
     },
     borderColor: {
       skin: {
-        line: withOpacity("--color-border"),
-        fill: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
+        line: "var(--color-border)",
+        fill: "var(--color-text-base)",
+        accent: "var(--color-accent)",
       },
     },
     fill: {
       skin: {
-        base: withOpacity("--color-text-base"),
-        accent: withOpacity("--color-accent"),
+        base: "var(--color-text-base)",
+        accent: "var(--color-accent)",
       },
       transparent: "transparent",
     },
