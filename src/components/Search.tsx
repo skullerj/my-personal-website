5dc0025e7368d9fa6f2ef9b808b54f0048799aee
Juan Rosero <juan@engineai.com>
2025-03-22 07:09:34 +0000

    fix: update tailwind version

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -82,9 +82,9 @@ export default function SearchBar({ searchList }: Props) {
           </svg>
         </span>
         <input
-          className="block w-full rounded border border-skin-fill 
-        border-opacity-40 bg-skin-fill py-3 pl-10
-        pr-3 placeholder:italic placeholder:text-opacity-75 
+          className="block w-full rounded border border-skin-fill/40 
+         bg-skin-fill py-3 pl-10
+        pr-3 placeholder:italic placeholder:text-skin-base/75
         focus:border-skin-accent focus:outline-none"
           placeholder="Search for anything..."
           type="text"
